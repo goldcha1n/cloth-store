596dedb58454092f19d96e46fa322c3e3e3ff818
goldcha1n <shcherbynayevhen24@gmail.com>
2023-06-07 11:04:51 +0300

    edit templates + add _base

--- products/views.py
+++ products/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
+from .models import ProductCategory, Product
+
 
 def index(request):
     return render(request, 'products/index.html')
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'categories': ProductCategory.objects.all()
+    }
+    return render(request, 'products/products.html', context)
