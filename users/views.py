2fa703f78e6406c35c801b24b9efeca5ddf91204
goldcha1n <shcherbynayevhen24@gmail.com>
2023-06-08 11:28:08 +0300

    login

--- users/views.py
+++ users/views.py
@@ -1,4 +1,9 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from users.forms import *
 
 
 def register(request):
@@ -6,7 +11,19 @@ def register(request):
 
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('products:index'))
+    else:
+        form = UserLoginForm()
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
 
 
 # def logout(request):
