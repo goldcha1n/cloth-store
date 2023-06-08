ed4f152e310c4e5405719b575f97aa89370c0c25
goldcha1n <shcherbynayevhen24@gmail.com>
2023-06-08 13:36:31 +0300

    changed urls

--- products/urls.py
+++ products/urls.py
@@ -5,6 +5,5 @@ from .views import *
 app_name = 'products'
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('products/', products, name='products'),
+    path('products/', products, name='index'),
 ]
