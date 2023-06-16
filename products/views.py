3b454bae756355861c4694b173ef062b5a7408df
goldcha1n <shcherbynayevhen24@gmail.com>
2023-06-16 17:15:48 +0300

    IndexView (CBV)

--- products/views.py
+++ products/views.py
@@ -2,13 +2,23 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from django.views.generic import TemplateView
 
 
 from .models import ProductCategory, Product, Basket
 
 
-def index(request):
-    return render(request, 'products/index.html')
+class IndexView(TemplateView):
+    template_name = 'products/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data()
+        context['title'] = 'Store'
+        return context
+
+
+# def index(request):
+#     return render(request, 'products/index.html')
 
 
 def products(request, category_id=None, page_number=1):
