09c3d44d6419ac303730c85945bf9f182ce1496e
goldcha1n <shcherbynayevhen24@gmail.com>
2023-06-16 11:30:07 +0300

    login_required

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
@@ -16,6 +17,7 @@ def products(request):
     return render(request, 'products/products.html', context)
 
 
+@login_required()
 def basket_add(request, product_id):
     product = Product.objects.get(id=product_id)
     baskets = Basket.objects.filter(user=request.user, product=product)
@@ -29,6 +31,7 @@ def basket_add(request, product_id):
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
 
+@login_required()
 def basket_delete(request, product_id):
     baskets = Basket.objects.filter(id=product_id)
     baskets.delete()
