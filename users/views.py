a90f9f86a715f7eb12e076ff16e55147752c2ee3
goldcha1n <shcherbynayevhen24@gmail.com>
2023-06-21 14:36:10 +0300

    users/views cleanup

--- users/views.py
+++ users/views.py
@@ -23,30 +23,9 @@ class UserRegistrationView(CreateView):
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')  # reverse_lazy, вместо reverse так как он не работает в классах
 
-# def login(request):
-#     if request.method == 'POST':
-#         form = UserLoginForm(data=request.POST)
-#         if form.is_valid():
-#             username = request.POST['username']
-#             password = request.POST['password']
-#             user = auth.authenticate(username=username, password=password)
-#             if user:
-#                 auth.login(request, user)
-#                 return HttpResponseRedirect(reverse('products:index'))
-#     else:
-#         form = UserLoginForm()
-#     context = {'form': form}
-#     return render(request, 'users/login.html', context)
-
 
 class UserLogoutView(LogoutView):
     template_name = 'users/login.html'
-
-
-@login_required()
-def logout(request):
-    auth.logout(request)
-    return HttpResponseRedirect(reverse('index'))
 
 
 class UserProfileView(UpdateView):
